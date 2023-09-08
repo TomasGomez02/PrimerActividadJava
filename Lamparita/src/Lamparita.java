22be326c5e056f8bc13077126b627be1cec971c8
TomasGomez02 <tagomez@estudiantes.unsam.edu.ar>
2023-09-08 17:54:35 -0300

    Hola

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,5 +1,30 @@
 public class Lamparita {
+    private boolean state;
+
+    public Lamparita()
+    {
+        state = false;
+    }
+
+    public void encender()
+    {
+        state = true;
+    }
+
+    public void apagar()
+    {
+        state = false;
+    }
+
+    public boolean getState()
+    {
+        return state;   
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Vamos a programar una lamparita! ");
+        Lamparita lam = new Lamparita();
+        System.out.println(lam.getState());
+        lam.encender();
+        System.out.println(lam.getState());
     }
 }
